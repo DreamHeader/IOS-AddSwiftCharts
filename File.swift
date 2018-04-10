79b2e7f86153bd33f9f1835ce0b052510064711a
IOS_FDK <fudongkaikaifa@163.com>
2018-04-10 18:11:50 +0800

    增加swiftCharts 文件
    123456

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  TestCharts
+//
+//  Created by gubeidianzi on 2018/4/10.
+//  Copyright © 2018年 dreamHeader. All rights reserved.
+//
+
+import Foundation
---
+++ TestCharts/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestCharts
+//
+//  Created by gubeidianzi on 2018/4/10.
+//  Copyright © 2018年 dreamHeader. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "TestCharts-Bridging-Header.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
